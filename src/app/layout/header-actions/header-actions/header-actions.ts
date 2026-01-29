3b7f3a65b1cd8880f25b33759cde6f9157d29e5f
Paulo Roberto De Morais <prmorais1302@gmail.com>
2026-01-29 11:14:21 -0300

    Creating pages component and adding buttons in toolbar

---
+++ src/app/layout/header-actions/header-actions/header-actions.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header-actions',
+  imports: [
+    MatButton,
+    MatIconButton,
+    MatIcon
+  ],
+  template: `
+    <div class="flex items-center gap-2">
+      <button matIconButton>
+        <mat-icon>favorite</mat-icon>
+      </button>
+      <button matIconButton>
+        <mat-icon>shopping_cart</mat-icon>
+      </button>
+      <button matButton>
+        Entrar
+      </button>
+      <button matButton="filled">
+        Cadastrar-se
+      </button>
+    </div>
+  `,
+  styles: ``,
+})
+export class HeaderActions {
+
+}
---
+++ src/app/pages/my-wushlist/my-wushlist.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-wushlist',
+  imports: [],
+  template: `
+    <p>
+      my-wushlist works!
+    </p>
+  `,
+  styles: ``,
+})
+export class MyWushlist {
+
+}
